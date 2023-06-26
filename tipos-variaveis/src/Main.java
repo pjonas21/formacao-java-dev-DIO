b3a2284ca7f4b16f7085d1898714c5bc51ea732b
Paulo Jonas <pjonas21@gmail.com>
2023-06-26 14:30:00 -0300

    inclusão de aulas

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double salario = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
     }
 }
